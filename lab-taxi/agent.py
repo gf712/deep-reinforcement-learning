9d69a15978a34a88b304c7671ea41c8d1c6c30d7
gf712 <gil_f.hoben@hotmail.com>
2019-07-23 16:10:23 +0100

    gym ai taxi solution - best score: 9.521

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, alpha=0.4, gamma=0.95, epsilon=1.0, epsilon_decay=2000, min_epsilon=0.005):
         """ Initialize agent.
 
         Params
@@ -12,8 +12,14 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self._epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
 
-    def select_action(self, state):
+    def select_action(self, env, state):
         """ Given the state, select an action.
 
         Params
@@ -24,8 +30,23 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
-
+        return np.random.choice(np.arange(self.nA), p=self._get_probabilities(state)) if state in self.Q \
+               else env.action_space.sample()
+    
+    def _get_probabilities(self, state):
+        
+        p = np.full(self.nA, self.epsilon / self.nA)
+        p[np.argmax(self.Q[state])] = 1 - self.epsilon + self.epsilon / self.nA
+        return p
+    
+    def update_epsilon(self, episode_number):
+        if episode_number % self.epsilon_decay == 0:
+            self.epsilon = self._epsilon
+        else:
+            self.epsilon = max((self.epsilon/2) * (np.cos(episode_number/self.epsilon_decay*np.pi)+1), self.min_epsilon)
+#         self.epsilon = max(self.epsilon * np.exp(-episode_number*self.epsilon_decay),
+#                            self.min_epsilon)
+    
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
@@ -37,4 +58,20 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        # Expected SARSA
+#         if not done:
+#             self.Q[state][action] = self.Q[state][action] + \
+#                                     self.alpha * (reward + self.gamma * sum(p * self.Q[next_state][i] for i, p in \
+#                                                                        enumerate(self._get_probabilities(next_state))) \
+#                                                   - self.Q[state][action])
+#         else:
+#             self.Q[state][action] = self.Q[state][action] + self.alpha * (reward - self.Q[state][action])
+        # Q-learning
+        if not done:
+            self.Q[state][action] = self.Q[state][action] + \
+                                    self.alpha * (reward + self.gamma * max(self.Q[next_state]) \
+                                                  - self.Q[state][action])
+        else:
+            self.Q[state][action] = self.Q[state][action] + self.alpha * (reward - self.Q[state][action])
+            
+        
--- lab-taxi/monitor.py
+++ lab-taxi/monitor.py
@@ -28,11 +28,12 @@ def interact(env, agent, num_episodes=20000, window=100):
     for i_episode in range(1, num_episodes+1):
         # begin the episode
         state = env.reset()
+        agent.update_epsilon(i_episode)
         # initialize the sampled reward
         samp_reward = 0
         while True:
             # agent selects an action
-            action = agent.select_action(state)
+            action = agent.select_action(env, state)
             # agent performs the selected action
             next_state, reward, done, _ = env.step(action)
             # agent performs internal updates based on sampled experience
